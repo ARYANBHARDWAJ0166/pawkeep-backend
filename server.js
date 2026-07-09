9e9ac02b69d85fa510e7164ae5f18a336cb092ce
ARYANBHARDWAJ0166 <rajivkumarsingh1236@gmail.com>
2026-07-09 20:13:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -19,8 +19,7 @@ app.use(cors({
     origin: [
         'http://localhost:8000',
         'http://127.0.0.1:5500',
-        'http://localhost:5500',
-        'https://pawkeep-frontend.onrender.com'
+        'https://pawkeep-frontend.vercel.app'
     ],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
