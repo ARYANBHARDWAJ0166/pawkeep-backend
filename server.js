db318c0fe14ce839ab09e3ed6f8334e862d985a5
ARYANBHARDWAJ0166 <rajivkumarsingh1236@gmail.com>
2026-07-09 19:46:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,12 @@ const uploadRoutes = require("./routes/uploadRoutes");
 const app = express();
 
 app.use(cors({
-    origin: ['http://localhost:8000', 'http://127.0.0.1:5500', 'http://localhost:5500'],
+    origin: [
+        'http://localhost:8000',
+        'http://127.0.0.1:5500',
+        'http://localhost:5500',
+        'https://pawkeep-frontend.vercel.app'
+    ],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
